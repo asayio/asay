339a4aa7aaaf3deabb0e8397057ac2a020f36184
sjwinther <sebastianwinther@gmail.com>
2017-07-17 00:10:35 +0200

    random icon (from selection) as logo on load

--- javascript/app/src/nav/Nav.js
+++ javascript/app/src/nav/Nav.js
@@ -4,13 +4,16 @@ import Logout from '../auth/Logout.js';
 import {
   Link
 } from 'react-router-dom';
-import { Zap } from 'react-feather';
+import { Bell, Cloud, Edit, Home, MessageCircle, MessageSquare, Moon, PieChart, Speaker, Camera, Droplet, Umbrella, Sun, ThumbsUp, Package, Map, Layers, Feather, Heart, Zap } from 'react-feather';
+
+const Icons = [Bell, Cloud, Edit, Home, MessageCircle, MessageSquare, Moon, PieChart, Speaker, Camera, Droplet, Umbrella, Sun, ThumbsUp, Package, Map, Layers, Feather, Heart, Zap]
+const RandomIcon = Icons[Math.floor(Math.random()*Icons.length)];
 
 class Nav extends Component {
   render () {
     return (
       <nav className="mb4">
-        <Link to="/" className="f3 link dark-pink hover-hot-pink ttl"><Zap className="svg-icon mr2" />Asay</Link>
+        <Link to="/" className="f3 link dark-pink hover-hot-pink ttl"><RandomIcon className="svg-icon mr2" />Asay</Link>
         {window.sessionStorage.authToken ? <Logout history={this.props.history}/> : <Login/>}
       </nav>
     )
