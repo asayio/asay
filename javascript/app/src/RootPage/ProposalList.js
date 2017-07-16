ed3765b1a716bd572ba0f34853e309c0ba2e8f9b
holgerthorup <holgerthorup@gmail.com>
2017-07-16 16:01:10 +0200

    proposal list update by filters

--- javascript/app/src/RootPage/ProposalList.js
+++ javascript/app/src/RootPage/ProposalList.js
@@ -7,38 +7,34 @@ class ProposalListSection extends Component {
   constructor() {
     super();
     this.state = {
-      proposalList:[],
+      proposals:[],
+      filteredProposals: [],
     };
-  }
+  };
 
-  async componentDidMount() {
-    const response = await fetch('/api/proposals');
-    const proposalList = await response.json();
-    this.setState({proposalList});
-  }
-
-  // {Object.proposals(this.state.proposalList)
-  //   // .filter(key => key.main == true)
-  //   .map((proposal, index) => {
-  //     return (
-  //       <Link key={proposal.id} to={`/proposal/${proposal.id}`}>
-  //         <div>
-  //           <h1>{proposal.ref} {proposal.subtitle}</h1>
-  //           <h2>({proposal.title})</h2>
-  //           <p>{proposal.session}</p>
-  //           <p>{proposal.status}</p>
-  //           <p>{proposal.duedate}</p>
-  //         </div>
-  //       </Link>
-  //     )
+  // async componentDidMount() {
+  //   console.log("hej");
+  //   const response = await fetch('/api/proposals');
+  //   const proposals = await response.json();
+  //   const filteredProposals = proposals
+  //   this.setState({proposals, filteredProposals});
+  // };
+  //
+  // filterProposals() {
+  //   const filteredProposals = this.state.proposals.filter(
+  //     function(proposal) {
+  //       return proposal.session === this.props.session
+  //     }
+  //   );
+  //   console.log(filteredProposals);
+  //   this.setState({filteredProposals: filteredProposals});
   //   }
-  // )}
 
   render() {
+      console.log(this.props.filteredProposals);
     return (
       <div>
-      {this.state.proposalList.filter(proposal => proposal.session = this.props.session)}
-      {this.state.proposalList.map((proposal, index) =>
+      {this.props.filteredProposals.map((proposal, index) =>
         <Link key={proposal.id} to={`/proposal/${proposal.id}`}>
           <div>
             <h1>{proposal.ref} {proposal.subtitle}</h1>
--- javascript/app/src/RootPage/index.js
+++ javascript/app/src/RootPage/index.js
@@ -5,6 +5,7 @@ class Root extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      proposals: [],  filteredProposals: [],
       sessions: [],   session: '',
       statuses: [],   status: '',
       tags: [],       tag: '',
@@ -14,13 +15,18 @@ class Root extends Component {
   }
 
   async componentDidMount() {
-    const response = await fetch('/api/lists');
-    const filterLists = await response.json();
+    const listResponse = await fetch('/api/lists');
+    const filterLists = await listResponse.json();
     const sessions = filterLists.sessions;
     const statuses = filterLists.status;
     const tags = filterLists.tags;
     const types = filterLists.types;
     this.setState({sessions, statuses, tags, types});
+
+    const proposalResponse = await fetch('/api/proposals');
+    const proposals = await proposalResponse.json();
+    const filteredProposals = proposals
+    this.setState({proposals, filteredProposals});
   };
 
   handleChange(event) {
@@ -29,6 +35,21 @@ class Root extends Component {
     const name = target.name;
     this.setState({
       [name]: value
+    }, () => {
+      const status = this.state.status;
+      const session = this.state.session;
+      const tag = this.state.tag;
+      const type = this.state.type;
+      const filteredProposals = this.state.proposals.filter(
+        function(proposal) {
+          return (status ? proposal.status === status : true)
+            && (session ? proposal.session === session : true)
+            && (tag ? proposal.tags === tag : true)
+            && (type ? proposal.type === type : true);
+        }
+      );
+      console.log(filteredProposals, "glo");
+      this.setState({filteredProposals: filteredProposals});
     });
   }
 
@@ -37,31 +58,28 @@ class Root extends Component {
       <main>
         <div>
           <select name="session" onChange={this.handleChange} value={this.state.session}>
-            {this.state.sessions.map((session, index) =>
+            {this.state.sessions.map((session) =>
               <option key={session.id}>{session.session}</option>
             )}
           </select>
           <select name="status" onChange={this.handleChange} value={this.state.status}>
-            {this.state.statuses.map((status, index) =>
+            {this.state.statuses.map((status) =>
               <option key={status.id}>{status.status}</option>
             )}
           </select>
           <select name="tag" onChange={this.handleChange} value={this.state.tag}>
-            {this.state.tags.map((tag, index) =>
+            {this.state.tags.map((tag) =>
               <option key={tag.id}>{tag.tag}</option>
             )}
           </select>
           <select name="type" onChange={this.handleChange} value={this.state.type}>
-            {this.state.types.map((type, index) =>
+            {this.state.types.map((type) =>
               <option key={type.id}>{type.type}</option>
             )}
           </select>
         </div>
         <ProposalListSection
-          session = {this.state.session}
-          status = {this.state.status}
-          tag = {this.state.tag}
-          type = {this.state.type}
+          filteredProposals = {this.state.filteredProposals}
         />
       </main>
     );
