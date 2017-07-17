d9bb01f6e702688995406c2417c77f75431804ff
Ian Victor Abildskou Kjær <Ivak@Ians-MacBook-Pro.local>
2017-07-17 20:34:26 +0200

    attempt to work in tags on the proposal list Have fun!

--- javascript/server/src/proposals/proposals.js
+++ javascript/server/src/proposals/proposals.js
@@ -1,13 +1,24 @@
 // Import
 const db = require('../../db.js');
+const getTags = require('../proposal/tags/proposalTags.js').getTags
 
 // Queries
 const selectProposals = db.sql('./src/proposals/selectProposals.sql');
 
 // Functions
+
 async function getProposals (request, response) {
-  const proposals = await db.cx.query(selectProposals);
-  response.send(proposals);
+  var proposalsSansTags = await db.cx.query(selectProposals);
+  // We need tags with the proposals on the proposal list so we can filter it
+  // I'm trying to do something like this, but proposalsWithTags just returns promise pending
+  // I'm missing something with these async calls...
+  //
+  // const proposalsWithTags = proposalsSansTags.map( async (proposal) => {
+  //   proposal.tags = await getTags(proposal.id);
+  //   return proposal;
+  // });
+  // response.send(proposalsWithTags);
+  response.send(proposalsSansTags);
 }
 
 // Export
