d6a859bd041058413176e5add612727a3d1207c5
holgerthorup <holgerthorup@gmail.com>
2017-07-18 14:58:24 +0200

    its not pretty but it works @fredrikbakke94

--- javascript/server/src/proposals/proposals.js
+++ javascript/server/src/proposals/proposals.js
@@ -6,19 +6,22 @@ const getTags = require('../proposal/tags/proposalTags.js').getTags
 const selectProposals = db.sql('./src/proposals/selectProposals.sql');
 
 // Functions
-
 async function getProposals (request, response) {
-  var proposalsSansTags = await db.cx.query(selectProposals);
-  // We need tags with the proposals on the proposal list so we can filter it
-  // I'm trying to do something like this, but proposalsWithTags just returns promise pending
-  // I'm missing something with these async calls...
-  //
-  // const proposalsWithTags = proposalsSansTags.map( async (proposal) => {
-  //   proposal.tags = await getTags(proposal.id);
-  //   return proposal;
-  // });
-  // response.send(proposalsWithTags);
-  response.send(proposalsSansTags);
+  var list = []
+  var proposalsSansTags = db.cx.query(selectProposals).then( async function(proposalsSansTags) {
+    var length = proposalsSansTags.length;
+    var counter = 1;
+    const proposalsWithTags = proposalsSansTags.map( async (proposal) => {
+      proposal.tags = await getTags(proposal.id);
+      list.push(proposal);
+      if (counter < length){
+        counter = counter +1;
+      } else {
+        response.send(list);
+      }
+      return proposal;
+    });
+  });
 }
 
 // Export
