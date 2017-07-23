5772eb78d137f5eb521db892261cba41aedecf21
Ian Victor Abildskou Kjær <Ivak@Ians-MacBook-Pro.local>
2017-07-23 14:24:06 +0200

    Mock up of an article meta data scrape service

---
+++ javascript/server/src/article/scrapeArticle.js
@@ -0,0 +1,17 @@
+// I imageine doing something like this
+// Call this service from the front end with an url as parameter
+//
+// Send back the meta data and let the front handle the request
+//
+// We need to find a good html header/meta data parser
+
+// import scrape from 'something-something-parser';
+//
+// async function scrapeArticle (request, response) {
+//   const url = request.url;
+//   scrape(url).then( (metaData) => {
+//     return metaData
+//   });
+// }
+//
+// module.exports = scrapeArticle
