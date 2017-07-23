39183a06cb979f83ba1b4b83f56134544b468719
holgerthorup <holgerthorup@gmail.com>
2017-07-23 13:01:14 +0200

    added tags to proposallist

--- javascript/app/src/proposalList/ProposalList.js
+++ javascript/app/src/proposalList/ProposalList.js
@@ -14,6 +14,7 @@ class ProposalListSection extends Component {
   };
 
   render() {
+    console.log(this.props.filteredProposals);
     return (
       <div>
       {this.props.filteredProposals.map((proposal, index) =>
@@ -21,9 +22,12 @@ class ProposalListSection extends Component {
           <div className="pt2 pb3 ph3 ba b--light-gray br2 mv3 lh-solid card ma1">
             <h2 className="f3">{proposal.ref}: {proposal.subtitle}</h2>
             <h3 className="f5 normal small-caps ttl silver mb4">
-              <b>Session: </b>{proposal.session},
-              <b>Status: </b> {proposal.status},
-              <b>Deadline: </b><CountDown dueDate = {proposal.duedate} />
+              <b>Session: </b>{proposal.session}
+              <b> Status: </b> {proposal.status}
+              <b> Deadline: </b><CountDown dueDate = {proposal.duedate} /><br/>
+              {proposal.tags.map((tag) =>
+                <span key={tag.id}> #{tag.tag}</span>
+              )}
             </h3>
             <p className="dark-gray">{proposal.title}</p>
           </div>
