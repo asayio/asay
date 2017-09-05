69ba583e26e554cbf61670e900dd8044a5c13337
holgerthorup <holgerthorup@gmail.com>
2017-09-05 11:08:17 +0200

    lovtekst.pdf + resume added to proposalinfo

--- app/src/proposalPage/ProposalInfo.js
+++ app/src/proposalPage/ProposalInfo.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import { File } from 'react-feather';
 // import Poll from '../widgets/poll/Poll';
 
 class ProposalInfo extends Component {
   render() {
     const proposal = this.props.proposalInfo;
-    console.log(proposal);
+    const ftSessionId = proposal.Periode.titel.substr(0,4) + '1';
+    const ftProposalId = proposal.nummer.replace(/\s/g,'');
+    const ftProposalSplit = ftProposalId.indexOf("A") > -1 || ftProposalId.indexOf("B") > -1 || ftProposalId.indexOf("C") > -1 ? true : false
+    const ftProposalIdOriginal = ftProposalSplit ? ftProposalId.substr(0,ftProposalId.length-1) : ftProposalId
     if (this.props.openDataCaseType && this.props.openDataStatus && this.props.openDataPeriod) {
       return (
         <div>
@@ -21,8 +25,16 @@ class ProposalInfo extends Component {
             <b>Status:</b> {proposal.Sagsstatus.status}
           </h2>
           <p className="dark-gray mt2 mb4">
-            {proposal.titel}
+            {proposal.resume}
           </p>
+          <a href={`http://www.ft.dk/ripdf/samling/${ftSessionId}/lovforslag/${ftProposalIdOriginal}/${ftSessionId}_${ftProposalIdOriginal}_som_fremsat.pdf`} target="blank" className="dib link dark-blue hover-blue v-btm mt3">
+            <File className="svg-icon mr1" />
+            <span className="lh-copy">Forslag som fremsat</span>
+          </a><br/>
+          <a href={`http://www.ft.dk/ripdf/samling/${ftSessionId}/lovforslag/${ftProposalId}/${ftSessionId}_${ftProposalId}_som_vedtaget.pdf`} target="blank" className="dib link dark-blue hover-blue v-btm mt3">
+            <File className="svg-icon mr1" />
+            <span className="lh-copy">Forslag som vedtaget</span>
+          </a>
           { /*
           <h3>Relevante dokumenter</h3>
           <ul>{this.props.attachments.map( (attachment) =>
