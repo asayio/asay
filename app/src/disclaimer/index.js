23c5ff4139f8e16304f3cf9565af107c6e1e827c
holgerthorup <holgerthorup@gmail.com>
2017-09-12 14:57:21 +0200

    discalimer page (html)

--- app/src/disclaimer/index.js
+++ app/src/disclaimer/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Logout from '../auth/Logout'
 import { Check } from 'react-feather';
 import { Link } from 'react-router-dom';
 
@@ -7,8 +8,11 @@ class NotFound extends Component {
     return (
       <div>
         <h1>Inden vi går i gang</h1>
-        <h2></h2>
+        <h2>Platformen er under udvikling</h2>
+        <p>Vi vil så gerne have feedback så vi kan lære af vores fejl og gøre det bedre næste gang. Derfor har vi valgt at give dig adgang til platformen mens vi stadig udvikler. Det betyder at funktionaliteten er begrænset og du risikrer at opleve fejl. Men det betyder også, at du kan være med til at forme platformen, så det opfylder dine behov. Tak fordi du vil være med.</p>
         <h2>Datasikkerhed og privatliv</h2>
+        <p>Dine oplysninger behandles efter gældende persondatalovgivning med Initiativet som databehandler. Vi har dit fornavn- og efternavn samt din email registret sammen med de stemmer du afgiver. Hvis du ikke er okay med det, så vent med at stem. Vi arbejder nemlig på en bedre, mere sikker og privat løsning. Det tager bare lidt længere tid. Tak for din tålmodighed.</p>
+        <Logout />
         <Link to="./" className="pointer link dark-blue hover-blue"><Check className="svg-icon"/>Accepter</Link>
       </div>
     );
