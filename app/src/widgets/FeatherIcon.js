bcf54a2939df44892790d487583d1600a41c7ad1
holgerthorup <holgerthorup@gmail.com>
2017-09-26 10:29:40 +0200

    prop defined feather icon widget

---
+++ app/src/widgets/FeatherIcon.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import * as Icon from 'react-feather'
+
+export default class FeatherIcon extends Component {
+  render () {
+    const CategoryIcon = Icon[this.props.name]
+    return(
+      <CategoryIcon/>
+    )
+  }
+}
