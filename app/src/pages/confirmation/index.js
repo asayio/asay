288c6f9ff75318ec71df0d801e879210cc3f3bb6
sjwinther <sebastianwinther@gmail.com>
2017-10-01 14:45:02 +0200

    vote styling

--- app/src/pages/confirmation/index.js
+++ app/src/pages/confirmation/index.js
@@ -3,10 +3,10 @@ import React, { Component } from 'react';
 class Confirmation extends Component {
   render() {
     return (
-      <div>
-        <h1>Din valghandling er registreret</h1>
-        <p>Luk vinduet for at vende tilbage forslaget</p>
-        <button onClick={() => window.close()}>Luk stemmeboks</button>
+      <div className="mw8 center tc">
+        <h1 className="f3 mt5 mb4">Din valghandling er registreret</h1>
+        <p>Luk vinduet for at vende tilbage forslaget.</p>
+        <a onClick={() => window.close()} className="pointer dib white bg-dark-blue hover-bg-blue mt3 pv2 ph4 ba b--black-10 br1 shadow-6">Luk stemmeboks</a>
       </div>
     );
   }
