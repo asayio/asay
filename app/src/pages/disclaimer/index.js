bc118a40e59f8528f1c2c9f50a1a63ad9a6ee0ca
sjwinther <sebastianwinther@gmail.com>
2017-10-06 12:46:20 +0200

    speed on disclaimer

--- app/src/pages/disclaimer/index.js
+++ app/src/pages/disclaimer/index.js
@@ -27,7 +27,7 @@ class Disclaimer extends Component {
         <h1 className="f3 mt5 mb4">Inden vi går i gang</h1>
         <div className="tl bg-white pa4 mb2 ba b--black-10 br1 shadow-6 lh-copy">
           <h2 className="f4">Platformen er under udvikling</h2>
-          <p>Vi vil så gerne have feedback så vi kan lære af vores fejl og gøre det bedre næste gang. Derfor har vi valgt at give dig adgang til platformen mens vi stadig udvikler. Det betyder at funktionaliteten er begrænset og du risikrer at opleve fejl. Men det betyder også, at du kan være med til at forme platformen, så det opfylder dine behov. Tak fordi du vil være med.</p>
+          <p>Vi vil så gerne have feedback så vi kan lære af vores fejl og gøre det bedre næste gang. Derfor har vi valgt at give dig adgang til platformen mens vi stadig udvikler. Det betyder at funktionaliteten er begrænset og du risikrer at opleve fejl. Det betyder, at loading hastigheden ikke er så hurtig, fordi al den information du læser er hentet live fra ft.dk. Men det betyder også, at du kan være med til at forme platformen, så det opfylder dine behov. Tak fordi du vil være med.</p>
           <h2 className="f4">Datasikkerhed og privatliv</h2>
           <p>Dine oplysninger behandles efter gældende persondatalovgivning med Initiativet som databehandler. Vi har dit fornavn- og efternavn samt din email registret sammen med de stemmer du afgiver. Hvis du ikke er okay med det, så vent med at stem. Vi arbejder nemlig på en bedre, mere sikker og privat løsning. Det tager bare lidt længere tid. Tak for din tålmodighed.</p>
         </div>
