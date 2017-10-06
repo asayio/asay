70f8cef2c8e2f60ba9774bbcf611eac45074e082
sjwinther <sebastianwinther@gmail.com>
2017-10-06 10:36:35 +0200

    ny forside

--- app/src/pages/root/index.js
+++ app/src/pages/root/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Login from '../../widgets/auth/Login'
-// import { Heart, UserPlus } from 'react-feather'
+import { Heart, UserPlus } from 'react-feather'
 import ProposalList from './ProposalList'
 
 class Root extends Component {
@@ -11,49 +11,46 @@ class Root extends Component {
       )
     } else {
       return (
-        <YC/>
+        <Welcome/>
       );
     }
   }
 }
 
-/*class Welcome extends Component {
+class Welcome extends Component {
   render() {
     return (
-      <div>
-        <h1>
-          Hvis alle kunne bestemme...<br/>
-          <span>Initiativets demokratiske platform til politik udvikling</span>
+      <div className="mw7 center tc">
+        <h1 className="f3 mt5 mb3">
+          Hvis alle kunne bestemme... <br/>
+          Initiativets demokratiske platform til politik udvikling
         </h1>
-
-        <p>Hos Initiativet er det dig der bestemmer. Hver gang. Og det gør du her.<br/></p>
-        <div>
-          <h2>Denne release</h2>
-          <span>Udgivet september 2017</span>
+        <p className="black-70">Hos Initiativet er det dig der bestemmer. Hver gang. Og det gør du her.</p>
+        <div className="tl bg-white pa4 mb2 ba b--black-10 br1 shadow-6 lh-copy">
+          <h2 className="mt2 mb0">Denne release</h2>
+          <span className="black-50">Udgivet september 2017</span>
           <ul>
             <li>Få hurtig indsigt i udvalgte forslag fra Folketinget</li>
             <li>Stem direkte på forslagene efter din overbevisning</li>
           </ul>
-        </div>
-        <div>
-          <h2>Næste release</h2>
-          <span>Forventet til november 2017</span>
+          <h2 className="mt2 mb0">Næste release</h2>
+          <span className="black-50">Forventet til november 2017</span>
           <ul>
             <li>Få let adgang til <i>alle</i> forslag fra Folketinget</li>
             <li>... uden at kende til Folketingets terminologi</li>
           </ul>
         </div>
-        <p>Platformen er stadig under udvikling, og adgang er derfor kun for medlemmer.</p>
-        <Login/><br/>
-        <a href="https://initiativet.net/membership" className="pointer dark-blue hover-blue"><UserPlus className="svg-icon mr2"/>Bliv medlem</a>
-        <p>Har du ikke modtaget login? <a href="mailto:dinevenner@initiativet.net" className="pointer dark-blue hover-blue">Send os en mail</a></p>
-        <footer><a href="https://asay.io" className="dark-pink ttl f7">Coded with <Heart className="svg-icon"/> by asay</a></footer>
+        <Login className="pointer db dib-ns white bg-dark-blue hover-bg-blue mt3 mr0 mr3-ns pv2 ph4 ba b--black-10 br1 shadow-6"/>
+        <a href="" className="pointer db dib-ns white bg-dark-blue hover-bg-blue mt3 pv2 ph4 ba b--black-10 br1 shadow-6"><UserPlus className="mr2"/>Opret bruger</a>
+        <p className="black-70 ma4">Problemer? <a href="mailto:dinevenner@initiativet.net" className="pointer dark-blue hover-blue">Send os en mail</a></p>
       </div>
     );
   }
-}*/
+}
 
-class YC extends Component {
+// <footer><a href="https://asay.io" className="dark-pink ttl f7">Coded with <Heart className="svg-icon"/> by asay</a></footer>
+
+/*class YC extends Component {
   render() {
     return (
       <div className="mw8 center tc">
@@ -68,6 +65,6 @@ class YC extends Component {
       </div>
     );
   }
-}
+}*/
 
 export default Root;
