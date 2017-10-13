461bf7f526450fcc6bd113893ce9d31a145c133c
sjwinther <sebastianwinther@gmail.com>
2017-10-13 16:00:29 +0200

    footer styling

--- app/src/widgets/Footer.js
+++ app/src/widgets/Footer.js
@@ -6,10 +6,10 @@ class Footer extends Component {
   render () {
     return (
       <footer className="mw8 center tc mv4">
-        <a target="_asay" href="https://asay.io" className="db dib-ns black-50 hover-black-70 ma2"><Circle/>Kodet som en del af Asay</a>
-        <a target="_asay" href="https://github.com/asayio/asay" className="db dib-ns black-50 hover-black-70 ma2"><Github/>Find os på Github</a>
-        <a target="_initiativet" href="https://initiativet.net" className="db dib-ns black-50 hover-black-70 ma2"><Edit3 />Giv en vælgererklæring</a>
-        <a target="_sponsor" href="https://initiativet.net/sponsor" className="db dib-ns black-50 hover-black-70 ma2"><CreditCard/>Bliv sponsor</a>
+        <a target="_initiativet" href="https://initiativet.net" className="db dib-ns black-50 hover-black-70 ma2"><Edit3 className="i-green"/>Giv en vælgererklæring</a>
+        <a target="_sponsor" href="https://initiativet.net/sponsor" className="db dib-ns black-50 hover-black-70 ma2"><CreditCard className="i-green"/>Bliv sponsor</a>
+        <a target="_asay" href="https://asay.io" className="db dib-ns black-50 hover-black-70 ma2"><Circle className="i-green"/>Kodet af Asay</a>
+        <a target="_github" href="https://github.com/asayio/asay" className="db dib-ns black-50 hover-black-70 ma2"><Github className="i-green"/>Find os på Github</a>
       </footer>
     )
   }
