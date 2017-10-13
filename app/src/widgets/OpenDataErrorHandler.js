bcd17307543fe1ce89230232b1d1980233f244b1
sjwinther <sebastianwinther@gmail.com>
2017-10-13 11:37:28 +0200

    styled error handling

--- app/src/widgets/OpenDataErrorHandler.js
+++ app/src/widgets/OpenDataErrorHandler.js
@@ -3,11 +3,11 @@ import React, { Component } from 'react';
 export default class OpenDataErrorHandler extends Component {
   render () {
     return (
-      <div>
-        <h3>Hov!</h3>
+      <div className="mw8 center tc">
+        <h3 className="f3 mv4">Hov!</h3>
         <p>Noget gik galt i folketingets EDB kælder :(</p>
-        <p>Prøv at genindlæse siden</p>
-        <button onClick={function() {window.location.reload()}}>Genindlæs siden</button>
+        <p>Prøv at genindlæse siden.</p>
+        <a onClick={function() {window.location.reload()}} className="pointer dib white bg-dark-blue hover-bg-blue mv3 pv2 ph3 ba b--black-10 br1 shadow-6">Genindlæs siden</a>
       </div>
     )
   }
