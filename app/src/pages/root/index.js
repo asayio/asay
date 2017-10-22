83ac41953bd5099556906be8fad262306cb8fc83
holgerthorup <holgerthorup@gmail.com>
2017-10-22 15:35:32 +0200

    front page release text update

--- app/src/pages/root/index.js
+++ app/src/pages/root/index.js
@@ -28,23 +28,24 @@ class Welcome extends Component {
     return (
       <div className="mw8 center tc">
         <h1 className="f3 mt5 mb3">
-          Initiativets demokratiske platform til politik udvikling
+          Velkommen til demokratifabrikken.
         </h1>
-        <p className="black-70">Hos Initiativet er det dig der bestemmer. Hver gang. Og det gør du her.</p>
+        <p className="black-70">Det er dig der bestemmer. Hver gang.</p>
         <Login icon="LogIn" iconClass="" type="login" className="pointer db dib-ns white bg-dark-blue hover-bg-blue mv2 mr0 mr3-ns pv2 ph4 ba b--black-10 br1 shadow-6"/>
         <Login icon="UserPlus" iconClass="" type="signUp" className="pointer db dib-ns white bg-dark-blue hover-bg-blue mv2 pv2 ph4 ba b--black-10 br1 shadow-6"/>
         <div className="tl bg-white pa4 mv3 ba b--black-10 br1 shadow-6 lh-copy">
           <h2 className="mt2 mb0">Denne release</h2>
-          <span className="black-50">Udgivet september 2017</span>
+          <span className="black-50">Udgivet oktober 2017</span>
           <ul>
-            <li>Få hurtig indsigt i udvalgte forslag fra Folketinget</li>
-            <li>Stem direkte på forslagene efter din overbevisning</li>
+            <li>Stor performanceforbedring for mere smidig brugeroplevelse</li>
+            <li>Lette udforskningsmuligheder blandt alle aktuelle forslag</li>
+            <li>Personligt fokus på de forslag, der er vigtige for dig</li>
           </ul>
           <h2 className="mt2 mb0">Næste release</h2>
-          <span className="black-50">Forventet til november 2017</span>
+          <span className="black-50">Forventet til december 2017</span>
           <ul>
-            <li>Få let adgang til <i>alle</i> forslag fra Folketinget</li>
-            <li>... uden at kende til Folketingets terminologi</li>
+            <li>Påmindelser om nye forslag og snarlige deadlines</li>
+            <li>Simple afstemningsresultater på afsluttede forslag</li>
           </ul>
         </div>
         <p className="black-70 ma4">Problemer? <a href="mailto:dinevenner@initiativet.net" className="pointer dark-blue hover-blue">Send os en mail</a></p>
