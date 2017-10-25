5d51bef68b86ec754c3e030ab1a92364819be3f7
sjwinther <sebastianwinther@gmail.com>
2017-10-25 20:00:57 +0200

    pref styling

--- app/src/pages/onboarding/index.js
+++ app/src/pages/onboarding/index.js
@@ -1,6 +1,8 @@
 import R from 'ramda'
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import PreferenceList from '../preferences/PreferenceList'
+import { CheckSquare } from 'react-feather';
 
 class Onboarding extends Component {
   componentDidMount() {
@@ -10,14 +12,17 @@ class Onboarding extends Component {
   render () {
     const workload = R.sum(R.pluck('workload')(R.filter(preferenceList => preferenceList.preference === true)(this.props.preferenceList)));
     return(
-      <div>
-        <h1 className="tc">Velkommen</h1>
-        <div className="mw8 center bg-white pa4 ba b--black-10 br1 shadow-6">
-          <div className="tc mb4">
+      <div className="mw8 center w-100 flex-auto">
+        <h1 className="f3 tc mt5 mb3">Velkommen</h1>
+        <div className="bg-white pa4 ba b--black-10 br1 shadow-6">
+          <div className="lh-copy mb4">
             <p>Vi vil gerne hjælpe dig med at navigere i alle de forslag, der bliver fremsat i Folketinget. Vælg de politiske emner der interesserer dig, så samler vi et overblik til dig. Du kan altid opdatere dine præferencer samt gennemsøge alle forslag.</p>
             <p>Med dine nuværende præferencer, skal du i gennemsnit tage stilling til ca. <b>{workload}</b> forslag om året. Det svarer til <b>{Math.ceil(workload / 52, 0) }</b> om ugen.</p>
           </div>
           <PreferenceList updateState={this.props.updateState} preferenceList={this.props.preferenceList}/>
+        </div>
+        <div className="tc mt3">
+          <Link to="/" className="db dib-ns b white bg-dark-blue hover-bg-blue pv2 ph4 ba b--black-10 br1 shadow-6"><CheckSquare className="mr2"/>Gå til lovforslag</Link>
         </div>
       </div>
     );
--- app/src/pages/preferences/index.js
+++ app/src/pages/preferences/index.js
@@ -1,6 +1,8 @@
 import R from 'ramda'
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import PreferenceList from './PreferenceList'
+import { CheckSquare } from 'react-feather';
 
 class Preferences extends Component {
   componentDidMount () {
@@ -10,14 +12,17 @@ class Preferences extends Component {
   render () {
     const workload = R.sum(R.pluck('workload')(R.filter(preferenceList => preferenceList.preference === true)(this.props.preferenceList)));
     return(
-      <div>
-        <h1 className="tc">Dine præferencer</h1>
-        <div className="mw8 center bg-white pa4 ba b--black-10 br1 shadow-6">
-          <div className="tc mb4">
+      <div className="mw8 center w-100 flex-auto">
+        <h1 className="f3 tc mt5 mb3">Dine præferencer</h1>
+        <div  className="bg-white pa4 ba b--black-10 br1 shadow-6">
+          <div className="lh-copy mb4">
             <p>Nedenfor kan du vælge, hvilke emner du vil følge med i.</p>
             <p>Med dine nuværende præferencer, skal du i gennemsnit tage stilling til ca. <b>{workload}</b> forslag om året. Det svarer til <b>{Math.ceil(workload / 52, 0) }</b> om ugen.</p>
           </div>
           <PreferenceList updateState={this.props.updateState} preferenceList={this.props.preferenceList}/>
+        </div>
+        <div className="tc mt3">
+          <Link to="/" className="db dib-ns b white bg-dark-blue hover-bg-blue pv2 ph4 ba b--black-10 br1 shadow-6"><CheckSquare className="mr2"/>Gå til lovforslag</Link>
         </div>
       </div>
     );
