7ee0e562e292b79b44b3a7d41239fd83b6eba28e
holgerthorup <holgerthorup@gmail.com>
2017-11-26 23:03:45 +0100

    added remember user auth lock language

--- app/src/widgets/auth/i18n_da.js
+++ app/src/widgets/auth/i18n_da.js
@@ -1,29 +1,31 @@
 export default {
   error: {
     login: {
-      'lock.fallback': 'Vi beklager, men der skete en fejl ved login. Prøv igen.',
-      'lock.invalid_email_password': 'Forkert e-mail eller adgangskode',
-      'lock.network': 'Vi kunne ikke få forbindelse til serveren. Kontroller venligst din forbindelse og prøv igen.',
-      'lock.unauthorized': 'Denne bruger har ikke adgang.',
-    },
+      "lock.fallback": "Vi beklager, men der skete en fejl ved login. Prøv igen.",
+      "lock.invalid_email_password": "Forkert e-mail eller adgangskode",
+      "lock.network": "Vi kunne ikke få forbindelse til serveren. Kontroller venligst din forbindelse og prøv igen.",
+      "lock.unauthorized": "Denne bruger har ikke adgang."
+    }
   },
   success: {
-    forgotPassword: 'Vi har sendt dig en e-mail med et link til at nulstille dit kodeord.',
+    forgotPassword: "Vi har sendt dig en e-mail med et link til at nulstille dit kodeord."
   },
-  blankErrorHint: 'Du mangler at skrive noget',
-  emailInputPlaceholder: 'email@email.com',
-  loginSubmitLabel: 'Log ind',
-  passwordInputPlaceholder: 'Kodeord',
-  title: 'Initiativet',
-  forgotPasswordTitle: '',
-  showPassword: 'Vis kodeord',
-  loginLabel: 'Log ind',
-  signUpLabel: 'Opret bruger',
-  signUpSubmitLabel: 'Opret bruger',
-  databaseAlternativeSignUpInstructions: 'eller',
-  databaseEnterpriseAlternativeLoginInstructions: 'eller',
-  loginWithLabel: 'Log ind med %s',
-  signUpWithLabel: 'Opret bruger med %s',
-  forgotPasswordAction: 'Glemt kodeord?',
-  forgotPasswordInstructions: 'Indtast din e-email. Vi sender dig en e-mail med et link til at nulstille dit kodeord.',
+  blankErrorHint: "Du mangler at skrive noget",
+  emailInputPlaceholder: "email@email.com",
+  loginSubmitLabel: "Log ind",
+  passwordInputPlaceholder: "Kodeord",
+  title: "Initiativet",
+  forgotPasswordTitle: "",
+  showPassword: "Vis kodeord",
+  loginLabel: "Log ind",
+  signUpLabel: "Opret bruger",
+  signUpSubmitLabel: "Opret bruger",
+  databaseAlternativeSignUpInstructions: "eller",
+  databaseEnterpriseAlternativeLoginInstructions: "eller",
+  loginWithLabel: "Log ind med %s",
+  signUpWithLabel: "Opret bruger med %s",
+  forgotPasswordAction: "Glemt kodeord?",
+  forgotPasswordInstructions: "Indtast din e-email. Vi sender dig en e-mail med et link til at nulstille dit kodeord.",
+  lastLoginInstructions: "Sidste gang loggede du ind som",
+  notYourAccountAction: "Er det ikke din konto?"
 };
