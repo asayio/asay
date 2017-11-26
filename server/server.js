dd7fc743b5ae50e0288ee6b012f71433f9674cf1
holgerthorup <holgerthorup@gmail.com>
2017-11-26 12:40:48 +0100

    stop ftBatchFethcher from running in dev

--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
 // Read .env file
-const environment = process.env.NODE_ENV || 'development';
-require('dotenv').config({path: `./.env.${environment}`});
+const environment = process.env.NODE_ENV || "development";
+require("dotenv").config({ path: `./.env.${environment}` });
 
 // Load error logging
-const Rollbar = require('rollbar');
+const Rollbar = require("rollbar");
 const rollbar = new Rollbar({
   accessToken: process.env.ROLLBAR,
   captureUncaught: true,
@@ -11,29 +11,31 @@ const rollbar = new Rollbar({
 });
 
 // Import
-const routes = require('./src/routes.js');
+const routes = require("./src/routes.js");
 
 // Variables
-const express = require('express');
+const express = require("express");
 const port = 3001; // Note: must match port of the "proxy" URL in app/package.json
 const app = express();
 
-const bodyParser = require('body-parser')
-app.use(bodyParser.json())
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
 
 routes.map(app);
 
-const path = require('path')
-app.use(express.static('app')); // Note: serve app as static assets
-app.get('*', function (request, response) {
-  response.sendFile(path.join(__dirname, './app/index.html'));
-})
+const path = require("path");
+app.use(express.static("app")); // Note: serve app as static assets
+app.get("*", function(request, response) {
+  response.sendFile(path.join(__dirname, "./app/index.html"));
+});
 
 // Initate webserver
-function listeningHandler () {
+function listeningHandler() {
   console.log(`Server is listening on port ${port}. Environment set to ${environment}.`);
-  const ftBatchFetcher = require('./src/integrations/ft/ftBatchFetcher')
-  ftBatchFetcher() // initial load
-  setInterval(ftBatchFetcher, 1000 * 60 * 60 * 24)
+  const ftBatchFetcher = require("./src/integrations/ft/ftBatchFetcher");
+  if (environment === "production") {
+    ftBatchFetcher(); // initial load
+    setInterval(ftBatchFetcher, 1000 * 60 * 60 * 24);
+  }
 }
 app.listen(port, listeningHandler);
