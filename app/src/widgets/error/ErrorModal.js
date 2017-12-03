1f946bb3f4e1e2c06c9e6955f92051f62227ec4e
holgerthorup <holgerthorup@gmail.com>
2017-12-03 18:43:36 +0100

    update error modal styling

--- app/src/widgets/error/ErrorModal.js
+++ app/src/widgets/error/ErrorModal.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import './style.css';
 
 class ErrorModal extends Component {
   closeModal(event) {
@@ -10,14 +9,27 @@ class ErrorModal extends Component {
     body.classList.remove('overflow-hidden');
   }
 
-  render () {
-    return(
-      <div id="error-modal" className="items-center justify-center overflow-auto w-100 h-100 pa2 z-9999 dn">
+  render() {
+    return (
+      <div
+        id="error-modal"
+        className="fixed absolute--fill bg-black-50 items-center justify-center overflow-auto pa2 z-9999 dn">
         <div className="pa3 pv4-ns ph5-ns tc bg-white ba b--black-10 br1">
           <h2 className="f4">Der er sket en fejl</h2>
-          <p>Det er ikke dig, det er os. Prøv igen, og hvis det stadig ikke virker så <a href="mailto:dinevenner@initiativet.net" target="_mailto" rel="noopener noreferrer" className="dark-blue hover-blue">send os en mail</a>.</p>
+          <p>
+            Det er ikke dig, det er os. Prøv igen, og hvis det stadig ikke virker så{' '}
+            <a
+              href="mailto:dinevenner@initiativet.net"
+              target="_mailto"
+              rel="noopener noreferrer"
+              className="dark-blue hover-blue">
+              send os en mail
+            </a>.
+          </p>
           <div>
-            <a onClick={this.closeModal} className="pointer dib dark-blue w4 pv2 ma2 ba b--dark-blue br1">OK</a>
+            <a onClick={this.closeModal} className="pointer dib dark-blue w4 pv2 ma2 ba b--dark-blue br1">
+              OK
+            </a>
           </div>
         </div>
       </div>
