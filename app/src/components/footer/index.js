ed2e99b96e284ca5443c823bad70ed8c1551784a
holgerthorup <holgerthorup@gmail.com>
2017-12-27 18:53:59 +0100

    'write us on slack' added to footer

--- app/src/components/footer/index.js
+++ app/src/components/footer/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Github, Edit3, CreditCard } from 'react-feather';
+import { Github, Edit3, CreditCard, Slack } from 'react-feather';
 
 class Footer extends Component {
   render() {
@@ -10,6 +10,12 @@ class Footer extends Component {
         </a>
         <a target="_sponsor" href="https://initiativet.dk/sponsor" className="db dib-ns black-50 hover-black-70 ma2">
           <CreditCard className="black-70 mr2" />Bliv sponsor
+        </a>
+        <a
+          target="_slack"
+          href="https://join.slack.com/t/initiativetdk/shared_invite/enQtMjkxNzMyNTIwNDY5LWYzY2UzN2E3NjQwN2FhYTdiM2NjMDk3ODZhYjUwNjNiZTYxNzM3NTc5MDdlNmRlOGY0MGZlN2U5NDM1ZWZjYjc"
+          className="db dib-ns black-50 hover-black-70 ma2">
+          <Slack className="black-70 mr2" />Skriv med os på Slack
         </a>
         <a target="_github" href="https://github.com/asayio/asay" className="db dib-ns black-50 hover-black-70 ma2">
           <Github className="black-70 mr2" />Find os på Github
