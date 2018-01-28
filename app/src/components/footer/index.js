978973c514d821a603eef21034a5f7784bb43e00
sjwinther <sebastianwinther@gmail.com>
2018-01-28 12:17:08 +0100

    footer items styled (without icons)

--- app/src/components/footer/index.js
+++ app/src/components/footer/index.js
@@ -1,24 +1,24 @@
 import React, { Component } from 'react';
-import { Github, Edit3, CreditCard, Slack } from 'react-feather';
+import FooterItem from '../footerItem';
 
 class Footer extends Component {
   render() {
     return (
       <footer>
-        <a target="_initiativet" href="https://initiativet.dk">
-          <Edit3 />Giv en vælgererklæring
-        </a>
-        <a target="_sponsor" href="https://initiativet.dk/sponsor">
-          <CreditCard />Bliv sponsor
-        </a>
-        <a
-          target="_slack"
-          href="https://join.slack.com/t/initiativetdk/shared_invite/enQtMjkxNzMyNTIwNDY5LWYzY2UzN2E3NjQwN2FhYTdiM2NjMDk3ODZhYjUwNjNiZTYxNzM3NTc5MDdlNmRlOGY0MGZlN2U5NDM1ZWZjYjc">
-          <Slack />Skriv med os på Slack
-        </a>
-        <a target="_github" href="https://github.com/asayio/asay">
-          <Github />Find os på Github
-        </a>
+        <div className="max-w-xl mx-auto text-center pt-8 pb-4">
+          <FooterItem
+            linkAddress="https://initiativet.dk"
+            linkTarget="_initiativet"
+            linkText="Giv en vælgererklæring"
+          />
+          <FooterItem linkAddress="https://initiativet.dk/sponsor" linkTarget="_sponsor" linkText="Bliv sponsor" />
+          <FooterItem
+            linkAddress="https://join.slack.com/t/initiativetdk/shared_invite/enQtMjkxNzMyNTIwNDY5LWYzY2UzN2E3NjQwN2FhYTdiM2NjMDk3ODZhYjUwNjNiZTYxNzM3NTc5MDdlNmRlOGY0MGZlN2U5NDM1ZWZjYjc"
+            linkTarget="_slack"
+            linkText="Skriv med os på Slack"
+          />
+          <FooterItem linkAddress="https://github.com/asayio/asay" linkTarget="_github" linkText="Find os på Github" />
+        </div>
       </footer>
     );
   }
---
+++ app/src/components/footerItem/index.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import FeatherIcon from '../featherIcon';
+
+class FooterItem extends Component {
+  render() {
+    return (
+      <a
+        target={this.props.linkTarget}
+        href={this.props.linkAddress}
+        className="inline-block text-grey-dark hover:text-grey-darkest mx-4 my-2">
+        {/* <FeatherIcon name={this.props.iconName} className="mr-2" /> */}
+        {this.props.linkText}
+      </a>
+    );
+  }
+}
+
+export default FooterItem;
