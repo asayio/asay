9df2c1f97a5de734b04faf877cabbdf932cca76a
sjwinther <sebastianwinther@gmail.com>
2018-04-01 15:54:08 +0200

    schtyling

--- app/src/components/modal/index.js
+++ app/src/components/modal/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import FeatherIcon from '../featherIcon';
 import './style.css';
 
 class Modal extends Component {
@@ -6,11 +7,11 @@ class Modal extends Component {
     return (
       <div id="modal" className="modal flex fixed z-50 pin items-center justify-center bg-black-50 p-2">
         <div className="relative w-full sm:w-128 text-center bg-white rounded-sm shadow p-8">
-          <div
+          <button
             onClick={() => this.props.updateState({ entityType: 'modal', entity: false })}
-            className="text-right mr-2">
-            <span className="pointer mid-gray hover-dark-gray f3 pl3 pb2">✕</span>
-          </div>
+            className="absolute pin-t pin-r leading-none hover:bg-grey-lightest rounded-sm p-1 m-1">
+            <FeatherIcon name="X" />
+          </button>
           {this.props.children}
         </div>
       </div>
