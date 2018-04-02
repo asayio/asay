eff52faa888dd9576087dcf939e3dc1c14d60038
sjwinther <sebastianwinther@gmail.com>
2018-04-02 13:28:40 +0200

    added results email template

--- server/src/mail/resultsMailBatch.js
+++ server/src/mail/resultsMailBatch.js
@@ -1,8 +1,8 @@
 // Import
 const apiKey = process.env.SENDINBLUE;
-const db = require('../db/db')
-const R = require('ramda')
-const sendinblue = require('sendinblue-api')
+const db = require('../db/db');
+const R = require('ramda');
+const sendinblue = require('sendinblue-api');
 
 // Functions
 async function resultsMailBatch(proposal) {
@@ -10,25 +10,27 @@ async function resultsMailBatch(proposal) {
   for (const vote of voteList) {
     const [user] = await db.cx.query('select * from public.user where id = ' + vote.user_id);
     if (user.result_notification) {
-      const client = new sendinblue({apiKey, timeout: 5000})
-      const body = 'Der er kommet resultater på lovforslaget ' + proposal.shortTitel + '. <br><br><a href="https://app.initiativet.dk/proposal/' + proposal.id + '">Klik her for at se resultaterne.</a>'
+      const client = new sendinblue({ apiKey, timeout: 5000 });
+      const body =
+        'Der er kommet resultater på lovforslaget ' +
+        proposal.shortTitel +
+        '. <br><br><a href="https://app.initiativet.dk/proposal/' +
+        proposal.id +
+        '">Klik her for at se resultaterne.</a>';
       const data = {
-        "to": {
+        to: {
           [user.email]: user.firstname
         },
-    		"from": [
-          "dinevenner@initiativet.dk",
-          "Initiativet"
-        ],
-    		"subject": "Der er kommet resultater på et lovforslag du har stemt på!",
-    		"html": body
-    	}
+        from: ['dinevenner@initiativet.dk', 'Initiativet'],
+        subject: 'Der er kommet resultater på et lovforslag, du har stemt på!',
+        html: body
+      };
       client.send_email(data, function(err, response) {
         console.log(response);
-      })
+      });
     }
   }
 }
 
 // Export
-module.exports = resultsMailBatch
+module.exports = resultsMailBatch;
