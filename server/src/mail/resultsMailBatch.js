497431df7f2bb27a9346faf5d4b10e543b907c30
holgerthorup <holgerthorup@gmail.com>
2018-04-02 15:04:59 +0200

    added variables to results mail batch template

--- server/src/mail/resultsMailBatch.js
+++ server/src/mail/resultsMailBatch.js
@@ -3,31 +3,45 @@ const apiKey = process.env.SENDINBLUE;
 const db = require('../db/db');
 const R = require('ramda');
 const sendinblue = require('sendinblue-api');
+const fs = require('fs');
+const cheerio = require('cheerio');
 
 // Functions
-async function resultsMailBatch(proposal) {
-  const voteList = await db.cx.query('select * from vote where proposal_id = ' + proposal.id);
-  for (const vote of voteList) {
-    const [user] = await db.cx.query('select * from public.user where id = ' + vote.user_id);
-    if (user.result_notification) {
-      const client = new sendinblue({ apiKey, timeout: 5000 });
-      const body =
-        'Der er kommet resultater på lovforslaget ' +
-        proposal.shortTitel +
-        '. <br><br><a href="https://app.initiativet.dk/proposal/' +
-        proposal.id +
-        '">Klik her for at se resultaterne.</a>';
-      const data = {
-        to: {
-          [user.email]: user.firstname
-        },
-        from: ['dinevenner@initiativet.dk', 'Initiativet'],
-        subject: 'Der er kommet resultater på et lovforslag, du har stemt på!',
-        html: body
-      };
-      client.send_email(data, function(err, response) {
-        console.log(response);
-      });
+async function resultsMailBatch() {
+  const proposalList = await db.cx.query('select * from proposal');
+  for (const proposal of proposalList) {
+    const voteList = await db.cx.query('select * from vote where proposal_id = ' + proposal.id);
+    for (const vote of voteList) {
+      const [user] = await db.cx.query('select * from public.user where id = ' + vote.user_id);
+      if (user.result_notification) {
+        fs.readFile(__dirname + '/newResultsTemplate.html', 'utf8', function(err, html) {
+          if (err) {
+            throw err;
+          }
+          var client = new sendinblue({ apiKey, timeout: 5000 });
+          const htmlNode = cheerio.load(html);
+          htmlNode('#header-proposal-title').html(proposal.data.shortTitel);
+          htmlNode('#name').html(user.firstname);
+          htmlNode('#proposal-title').html(proposal.data.shortTitel.toLowerCase());
+          htmlNode('#proposal-button').after(
+            '<a id="proposal-button-link" style="text-decoration:none; color:#ffffff; font-weight:normal;" target="_blank" href="' +
+              'https://app.initiativet.dk/proposal/' +
+              proposal.id +
+              '"> <strong>Se resultaterne for forslaget</strong></a>'
+          );
+          const data = {
+            to: {
+              [user.email]: user.firstname
+            },
+            from: ['dinevenner@initiativet.dk', 'Initiativet'],
+            subject: 'Der er kommet nye lovforslag til dig!',
+            html: htmlNode.html()
+          };
+          client.send_email(data, function(err, response) {
+            console.log(response);
+          });
+        });
+      }
     }
   }
 }
