c5f62a9a99e3ae7104f631daabb29b1aaa4cef64
Sebastian Winther <sebastianwinther@gmail.com>
2018-09-07 14:23:17 +0200

    third paty cookie notice on err 401

--- app/src/routes/401/index.js
+++ app/src/routes/401/index.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import Login from '../../components/loginBtn';
+import React, { Component } from "react";
+import Login from "../../components/loginBtn";
 
 class Curious extends Component {
   render() {
@@ -7,10 +7,14 @@ class Curious extends Component {
       <div className="flex-auto px-2">
         <div className="max-w-xl mx-auto text-center">
           <h1>Ups! Der er problemer</h1>
-          <p className="mx-auto">Vi kan ikke genkende dig. Prøv at logge ind (igen).</p>
+          <p className="mx-auto">
+            Vi kan ikke genkende dig. Måske er det fordi du har slået
+            tredjepartscookies fra i din webbrowser? Slå dem til, og prøv at
+            logge ind (igen).
+          </p>
           <Login type="login" className="btn btn-white mt-4 mb-8" />
           <p className="mx-auto">
-            Virker det stadig ikke?{' '}
+            Virker det stadig ikke?{" "}
             <a href="mailto:dinevenner@initiativet.dk" className="inline-link">
               Send os en mail
             </a>.
